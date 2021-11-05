fa5282f3e562af09ce0c12d43b34b694c7c13a7d
jzx <945627077@qq.com>
2021-11-05 14:35:26 +0800

    增加token生成器增加id和ttl

--- app/user/UserController.php
+++ app/user/UserController.php
@@ -3,12 +3,14 @@
 namespace App\User;
 
 use App\Base\BaseController;
-
+use App\Utils\Uuid;
 
 class UserController extends  BaseController {
     
     static public function  index(){
         
-        return self::success("");
+        return self::success([
+            "token"=>Uuid::token(1)
+        ]);
     }
 }
--- app/utils/Uuid.php
+++ app/utils/Uuid.php
@@ -5,8 +5,17 @@ namespace App\Utils;
 
 class UUid 
 {
+    /**
+     * 生成跟踪id
+     */
     static function tid(){
         return base_convert(date("ymdhis",time()).rand(),10,31);
+    }
+    /**
+     * 生成token 1秒钟生成一次 默认生存周期 7200秒
+     */
+    static function token($id,$ttl=7200){
+        return base_convert(date("sihdmy",time()).rand().$id."00".$ttl,10,31);
     }
 }
 
