0642b84e16fb2890f3cdcddde7284ccba61217de
jzx <945627077@qq.com>
2021-11-11 12:28:02 +0800

    文档初始化

--- site/pysto/components/layout.js
+++ site/pysto/components/layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <div>
       <Navbar />
-      <main>{children}</main>
+      {children}
       <Footer />
     </div>
   )
--- site/pysto/components/navbar.js
+++ site/pysto/components/navbar.js
@@ -3,8 +3,13 @@ import Link from 'next/link'
 export default function Footer({ children }) {
     return (
         <div className="container mx-auto flex ">
-            <div className="flex-grow py-2 font-bold">
-                <Link href="/" >Pysto</Link>
+            <div className="flex-grow py-2 flex">
+                <div className=" font-bold">
+                    <Link href="/" >Pysto</Link>
+                </div>
+                <div className="px-5">
+                    <Link href="/doc" >文档</Link>
+                </div>
             </div>
             <div className="py-2 flex  text-sm">
             <Link href="/login" >
