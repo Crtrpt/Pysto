6c6609fa8c14600be0bea21ceae2910dcb413ec2
jzx <945627077@qq.com>
2021-11-11 11:09:23 +0800

    增加验证组件

--- app/index/IndexController.php
+++ app/index/IndexController.php
@@ -6,12 +6,15 @@ use App\Base\BaseController;
 use App\Utils\Uuid;
 
 use Service\TestService;
-
+use Core\Validation;
 class IndexController extends  BaseController {
     /**
      * index api
      */
     static public function  index($ctx,$form){ 
+        Validation::test($form,[
+            "aaa"=>["required","is_email"]
+        ]);
         $t=new TestService();
         return self::success($t->test());
     }
---
+++ core/Validation.php
@@ -0,0 +1,12 @@
+<?php
+namespace Core;
+
+class Validation {
+    static function test($form,$test){
+        foreach($test as $k=>$rule){
+            foreach($rule as $r){
+                //验证xxx
+            }
+        }
+    }
+}
